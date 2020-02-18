9e5022d57bc45fa6ade8a1034c72d84cd4bfea8e
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-02-18 10:28:24 -0600

    able to see stories create route

--- resources/stories.py
+++ resources/stories.py
@@ -1,7 +1,7 @@
 import models
 
 #import Blueprint
-from flask import Blueprint
+from flask import Blueprint, request
 
 #get model as dict
 from playhouse.shortcuts import model_to_dict
@@ -11,7 +11,18 @@ stories = Blueprint('stories', 'stories')
 
 
 #GET /
-#index route 
+#INDEX route 
 @stories.route('/', methods=['GET'])
 def stories_index():
 	return "stories index"
+
+
+#CREATE route
+#POST /
+@stories.route('/', methods=['POST'])
+def create_stories():
+	#we need to get the JSON from the req
+	#request.get_json
+	payload = request.get_json()
+	print(payload)
+	return "stories create route"
