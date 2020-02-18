4763eb1ffa2c466062103184c714740a8f353e2a
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-02-17 20:26:44 -0600

    able to POST with user/register

--- resources/users.py
+++ resources/users.py
@@ -2,7 +2,7 @@
 import models
 
 #import Blueprint
-from flask import Blueprint
+from flask import Blueprint, request
 
 #user blueprint
 users = Blueprint('users', 'users')
@@ -13,3 +13,10 @@ users = Blueprint('users', 'users')
 @users.route('/', methods=['GET'])
 def test_user_resource():
   return "testing the user resource here"
+
+
+#POST /register
+@users.route('/register', methods=['POST'])
+def register():
+  print(request.get_json())
+  return "check terminal for request.get_son()"
