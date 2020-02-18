e7f104e9f573a4e7e80d1e917897ad4b4b626266
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-02-17 18:03:30 -0600

    added LoginManager to app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask 
 
+
+
+#uses flash to display messages when a user is required to log in
+from flask_login import LoginManager
+
+
 import models 
 
 
@@ -8,6 +14,17 @@ PORT = 8000
 
 # instantiating the Flask class
 app = Flask(__name__) 
+
+
+# need to set up a secret key
+app.secret_key = "Alpacas. Donkeys. Koalas. Zebras."
+
+#instantiate LoginManager to get a login_manager
+login_manager = LoginManager()
+
+#connect the app with the login manager
+login_manager.init_app(app)
+
 
 #ROUTES 
 
