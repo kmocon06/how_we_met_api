f5eff2cceaaae48955f830a11952b1aaf9fb28ad
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-02-18 11:09:45 -0600

    able to create a new story

--- resources/stories.py
+++ resources/stories.py
@@ -1,7 +1,10 @@
 import models
 
 #import Blueprint
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+
+#we need the current user that is logged in to see all the routes
+from flask_login import current_user
 
 #get model as dict
 from playhouse.shortcuts import model_to_dict
@@ -25,4 +28,25 @@ def create_stories():
 	#request.get_json
 	payload = request.get_json()
 	print(payload)
-	return "stories create route"
+
+	story = models.Story.create(user_id=current_user.id,title=payload['title'], 
+		story_content=payload['story_content'], image=payload['image'])
+
+	print(story.__dict__)
+
+	story_dict = model_to_dict(story)
+
+	#we don't need the password information here so we can pop it off the list
+	story_dict['user_id'].pop('password')
+
+
+	return jsonify(
+		data=story_dict,
+		message="New story was created!",
+		status= 201,
+	), 201
+
+
+
+
+
